2ec7fec5d225b7e5de28df6c66b78d7a74d92052
yoonseo <yo26on@soongsil.ac.kr>
2021-12-26 03:05:13 +0900

    1이 될 때까지 (#1)

---
+++ CodingTestBook/Chapter 3/3-4.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+
+count = 0
+
+while True:
+    if n == 1:
+        break
+    if n % k == 0:
+        n = n/k
+    else:
+        n = n - 1
+    count += 1
+
+print(count)
